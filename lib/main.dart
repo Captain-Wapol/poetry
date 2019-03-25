6f6949d0037de59d86c126d9c5e459d82393acb8
Captain-Wapol <875591882@qq.com>
2019-03-25 15:14:24 +0800

    去除debug标识

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Aimee's App",
+      debugShowCheckedModeBanner: false, 
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
